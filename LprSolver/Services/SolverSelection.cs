b5222ec0a98b0a943b12df318f7c58aa98c6da6b
Jacques <jacquesvnme@gmail.com>
2026-08-08 10:25:49 +0200

    Fixed returns for solvers

--- LprSolver/Services/SolverSelection.cs
+++ LprSolver/Services/SolverSelection.cs
@@ -67,42 +67,70 @@ public class SolverSelection : ISolverSelection
         {
             case SolverAlgorithm.PrimalSimplex:
                 var primal_simplex = await _primal_Simplex_Algorithm.Execute(linearProgram);
-                return (
+                return new(
                     primal_simplex.Success,
                     primal_simplex.Message,
                     primal_simplex.exportTableData
                 );
 
             case SolverAlgorithm.Revised_PrimalSimplex:
-                await _revised_Primal_Simplex_Algorithm.Execute(linearProgram);
-                return new(true, "Revised primal simplex completed.", null);
+                var revised_primal_simplex = await _revised_Primal_Simplex_Algorithm.Execute(linearProgram);
+                return new(
+                     revised_primal_simplex.Success,
+                     revised_primal_simplex.Message,
+                     revised_primal_simplex.exportTableData
+                 );
 
             case SolverAlgorithm.BranchAndBound:
-                await _b_B_Simplex_Algorithm.Execute(linearProgram);
-                return new(true, "Branch and bound completed.", null);
+                var branch_and_bound = await _b_B_Simplex_Algorithm.Execute(linearProgram);
+                return new(
+                     branch_and_bound.Success,
+                     branch_and_bound.Message,
+                     branch_and_bound.exportTableData
+                 );
 
             case SolverAlgorithm.Revised_BranchAndBound:
-                await _revised_B_B_Simplex_Algorithm.Execute(linearProgram);
-                return new(true, "Revised branch and bound completed.", null);
+                var revised_branch_and_bound = await _revised_B_B_Simplex_Algorithm.Execute(linearProgram);
+                return new(
+                     revised_branch_and_bound.Success,
+                     revised_branch_and_bound.Message,
+                     revised_branch_and_bound.exportTableData
+                 );
 
             case SolverAlgorithm.CuttingPlane:
-                await _cutting_Plane_Algorithm.Execute(linearProgram);
-                return new(true, "Cutting plane completed.", null);
+                var cutting_plane = await _cutting_Plane_Algorithm.Execute(linearProgram);
+                return new(
+                     cutting_plane.Success,
+                     cutting_plane.Message,
+                     cutting_plane.exportTableData
+                 );
 
             case SolverAlgorithm.Revised_CuttingPlane:
-                await _revised_Cutting_Plane_Algorithm.Execute(linearProgram);
-                return new(true, "Revised cutting plane completed.", null);
+                var revised_cutting_plane = await _revised_Cutting_Plane_Algorithm.Execute(linearProgram);
+                return new(
+                     revised_cutting_plane.Success,
+                     revised_cutting_plane.Message,
+                     revised_cutting_plane.exportTableData
+                 );
 
             case SolverAlgorithm.BranchAndBoundKnapsack:
-                await _b_B_Knapsack_Algorithm.Execute(linearProgram);
-                return new(true, "Branch and bound knapsack completed.", null);
+                var branch_and_bound_knapsack = await _b_B_Knapsack_Algorithm.Execute(linearProgram);
+                return new(
+                     branch_and_bound_knapsack.Success,
+                     branch_and_bound_knapsack.Message,
+                     branch_and_bound_knapsack.exportTableData
+                 );
 
             case SolverAlgorithm.NonLinearProblem:
-                await _nonLinearProblem.Execute(linearProgram);
-                return new(true, "Non-linear problem solver completed.", null);
+                var non_linear_problem = await _nonLinearProblem.Execute(linearProgram);
+                return new(
+                     non_linear_problem.Success,
+                     non_linear_problem.Message,
+                     non_linear_problem.exportTableData
+                 );
 
             default:
-                return new(false, "Unsupported solver algorithm.", null);
+                return new(false, "Invalid solver algorithm",  null);
         }
     }
 }
