5896237a0aa6ec007d47faf5ee3c74ff5a7655b4
Jacques <jacquesvnme@gmail.com>
2026-08-08 12:22:05 +0200

    Added extension class for StringBuilder.
    This is going to be used in exporter section.

---
+++ LprSolver/Services/StringBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace LprSolver.Extensions;
+
+public static class StringBuilderExtensions
+{
+    public static StringBuilder AppendTitle(this StringBuilder builder, string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            return builder;
+        }
+
+        builder.AppendLine("");
+        builder.AppendLine($"--- {title.Trim()} ---");
+        builder.AppendLine("");
+
+        return builder;
+    }
+
+    public static StringBuilder AppendListLines(
+        this StringBuilder builder,
+        IEnumerable<string> lines
+    )
+    {
+        foreach (string line in lines)
+        {
+            builder.AppendLine(line);
+        }
+
+        return builder;
+    }
+}
