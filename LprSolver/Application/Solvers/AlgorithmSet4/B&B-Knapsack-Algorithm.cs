81de2f975e14eca5e0ac3e051440a5c1acbc422e
Sarelda Nel <141139618+simonscrambled@users.noreply.github.com>
2026-08-14 12:33:53 +0200

    Class Knapsack - ratio - CalculateBound - Solve() -- root node implemented

--- LprSolver/Application/Solvers/AlgorithmSet4/B&B-Knapsack-Algorithm.cs
+++ LprSolver/Application/Solvers/AlgorithmSet4/B&B-Knapsack-Algorithm.cs
@@ -60,6 +60,85 @@ class Node
     }
 }
 
+class Knapsack
+{
+    private List<Item> item;
+    private int capacity;
+
+    // Stores the best solution found so far
+    private int bestValue = 0;
+
+    public Knapsack(List<Item> items, int capacity)
+    {
+        // Sort items by value/weight ratio
+        this.items = items
+            .OrderByDescending(item => item.Ratio) //sorting
+            .ToList; //adding to the list
+
+        this.capacity = capacity; //capacity of knapsack
+    }
+
+    //
+    private double CalculateBound(Node node)
+    {
+        // if the current weight already reaches/exceeds capacity, dont add anything else
+        if (node.Weight >= capacity)
+            return node.Value;
+
+        double bound = node.Weight;
+
+        // Weight currently being considerd
+        int totalWeight = node.Weight;
+
+        // Start with the next item in the ratio
+        int i = node.Level + 1;
+
+        // Add items while they fit in the knapsack
+        while (i < items.Count &&
+                totalWeight + item[i].Weight >= capacity)
+        {
+            totalWeight += item[i].Weight;
+            bound += item[i].Value;
+            i++;
+        }
+
+        // if next items remains, take a Fraction of it
+        // only for calculating the bound, items remain intergers
+        if (i < items.Count)
+        {
+            int remainingCapacity = capacity - totalWeight;
+            bound += remainingCapacity * items[i].Ratio;
+        }
+        return bound;
+    }
+    
+    // Solve Knapsack
+    public int Solve()
+    {
+        // keeps track of nodes that needs to be solved in order of smaller remaining capacity
+        var queue = new PriorityQueue<Node, int>();
+
+        //root node
+        Node root = new Node(
+            -1,// level, no items considerd yet
+            0,// weight, knapsack is empty
+            0// value, no value collected yet
+        );
+
+        // upper bound of the root
+        root.Bound = CalculateBound(root);
+
+        // remaining capacity left over at the root
+        int remainingCapacity = capacity - root.Weight;
+
+        // add root to the priority queue
+        queue.Enqueue(root, remainingCapacity);
+
+        // continue while there are nodes to be explored
+    }
+
+}
+
 public class B_B_Knapsack_Algorithm : IB_B_Knapsack_Algorithm
 {
     /// <summary>
