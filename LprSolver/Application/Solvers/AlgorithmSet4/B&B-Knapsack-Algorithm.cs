58c3f1e7d36c522773b1396ae3735d8d81597db0
Sarelda Nel <141139618+simonscrambled@users.noreply.github.com>
2026-08-14 11:51:09 +0200

    Classes for Knapsack

--- LprSolver/Application/Solvers/AlgorithmSet4/B&B-Knapsack-Algorithm.cs
+++ LprSolver/Application/Solvers/AlgorithmSet4/B&B-Knapsack-Algorithm.cs
@@ -1,4 +1,7 @@
 ﻿using LprSolver.Models;
+using System;
+using Sysyem.Collections.Generic;
+using System.Linq;
 
 namespace LprSolver.Application.Solvers.AlgorithmSet4;
 
@@ -10,6 +13,51 @@ public interface IB_B_Knapsack_Algorithm
     Task<(bool Success, string Message, ExportReport exportTableData)> Execute(
         LinearProgram linearProgram
     );
+}
+
+/// Classes for knapsack
+
+// An item in the knapsack
+class Item
+{
+    public int Weight { get; set; }
+    public int Value {  get; set; }
+    
+    //Value to Weight ratio
+    //Used when calculating the upper bound
+    public double Ratio
+    {
+        get { return (double)Value / Weight; }
+    }
+
+    public Item( int weight, int value) //constructure
+    {
+        Weight = weight;
+        Value = value;
+    }
+}
+
+// One node of the Branch and Bound Tree
+class Node
+{
+    // Intex of the last item considerd
+    public int Level { get; set; }
+
+    // Total Weight of items selected so far
+    public int Weight { get; set; }
+
+    // Total Value of the items so far
+    public int Value { get; set; }
+
+    // Upperbound: Maximum value that could be obtained from this node
+    public double Bound {  get; set; }
+
+    public Node(int level, int weight, int value)
+    {
+        Level = level;
+        Weight = weight;
+        Value = value;
+    }
 }
 
 public class B_B_Knapsack_Algorithm : IB_B_Knapsack_Algorithm
@@ -29,6 +77,7 @@ public class B_B_Knapsack_Algorithm : IB_B_Knapsack_Algorithm
         LinearProgram linearProgram
     )
     {
+        
         // Add code here to implement the Algorithm.
         // Keep in mind that the return data should match the expected output format for the application.
 
