3ff25c8fb9be7f4b8ac123c6784d5d953557218d
Jacques <jacquesvnme@gmail.com>
2026-08-23 11:17:26 +0200

    Added reusable utils for primal simplex

---
+++ LprSolver/Application/SolverUtils/PrimalSimplexUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace LprSolver.Application.SolverUtils;
+
+public static class PrimalSimplexUtils {
+    public static List<double> NormalizeObjective(List<double> values)
+    {
+        if (values == null || values.Count == 0)
+        {
+            throw new ArgumentException("Values cannot be null or empty.", nameof(values));
+        }
+
+        return values.Select(value => -value).ToList();
+    }
+}
