1d3b5a821c45c235276d0fd5fbda83e9015a5c04
Jacques <jacquesvnme@gmail.com>
2026-08-26 22:21:29 +0200

    Removed the default for revised cutting plane and added a expected failure return

--- LprSolver/Application/Solvers/AlgorithmSet3/Revised-Cutting-Plane-Algorithm.cs
+++ LprSolver/Application/Solvers/AlgorithmSet3/Revised-Cutting-Plane-Algorithm.cs
@@ -29,27 +29,14 @@ public class Revised_Cutting_Plane_Algorithm : IRevised_Cutting_Plane_Algorithm
         LinearProgram linearProgram
     )
     {
-        // Add code here to implement the Algorithm.
-        // Keep in mind that the return data should match the expected output format for the application.
-
-        // Call your own custom methods inside this class but make them private to avoid exposing them outside of this class.
-        OtherMethods();
-
-        var tables = new List<object>();
-
         var exportReport = new ExportReport
         {
             AdditionalData = new AdditionalData(),
             ImportantDetails = new ImportantDetails(),
             SensitivityAnalysis = new SensitivityAnalysis(),
-            Tables = new ExportTable { Tables = tables },
+            Tables = new ExportTable(),
         };
 
-        return new(true, "Dummy revised cutting plane table created successfully.", exportReport);
-    }
-
-    private void OtherMethods()
-    {
-        //dummy method
+        return new(false, "Method not implemented.", exportReport);
     }
 }
