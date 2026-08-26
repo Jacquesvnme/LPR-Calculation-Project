7ecabb1753e1f22d17ea6adb2c3698b256f6df07
Jacques <jacquesvnme@gmail.com>
2026-08-26 22:26:57 +0200

    Added basic empty class for dual simplex

---
+++ LprSolver/Application/SolverUtils/DualSimplexUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace LprSolver.Application.SolverUtils;
+
+public static class DualSimplexUtils
+{
+    // Floating-point calculations can produce very tiny negative values.
+    // These very small values should just be 0.
+    // The tolerance prevents those rounding errors from making the tableau appear infeasible.
+    private const double TOLERANCE = 0.0000001;
+    private const int MAXIMUM_ITERATIONS = 1000;
+}
